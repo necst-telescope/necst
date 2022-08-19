638da3df5e7a27349f540d852aa93abdd395e75f
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2022-08-19 18:02:48 +0900

    #1 Fix code style

--- necst/__init__.py
+++ necst/__init__.py
@@ -1,10 +1,11 @@
 from importlib.metadata import version
 
+import neclib
+
+
 try:
     __version__ = version("necst")
 except:  # noqa: E722
     __version__ = "0.0.0"
-
-import neclib
 
 config = neclib.config
