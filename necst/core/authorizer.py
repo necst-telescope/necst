1b0f345d2046ee1506782e818efe70cd5a4cdf39
KaoruNishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2022-08-30 01:49:55 +0000

    #14 Implement authority approval method

--- necst/core/authorizer.py
+++ necst/core/authorizer.py
@@ -1,34 +1,70 @@
-from typing import NoReturn
+from typing import Final, Optional
 
+import rclpy
 from rclpy.node import Node
 
 from necst import config
-from necst_msgs.msg import AuthoritySrv
+from necst_msgs.srv import AuthoritySrv
 
 
 class Authorizer(Node):
 
-    NodeName = "authorizer"
+    NodeName: Final[str] = "authorizer"
     NameSpace = f"/necst/{config.observatory}/core/auth"
 
     def __init__(self) -> None:
         super().__init__(self.NodeName, namespace=self.NameSpace)
         self.logger = self.get_logger()
-        # self.pub = self.create_publisher(...)
-        # self.sub = self.create_subscription(..., self.destroy_this)
-        # self._check_singleton()
+        self._check_singleton()
 
-        self.srv = self.create_service(AuthoritySrv, "request", self.authorize)
+        self.__approved: Optional[str] = None
 
-    def authorize(
+        self.srv = self.create_service(AuthoritySrv, "request", self._authorize)
+
+    @property
+    def approved(self) -> Optional[str]:
+        return self.__approved
+
+    def _authorize(
         self, request: AuthoritySrv.Request, response: AuthoritySrv.Response
     ) -> AuthoritySrv.Response:
-        response.approval = request.requester
+        if not request.requester:
+            self.logger.debug("Unregister privileged node")
+            self.__approved = None
+            response.approval = True
+        elif self.__approved is None:
+            self.logger.debug("Approve privilege request")
+            self.__approved = request.requester
+            response.approval = True
+        else:
+            self.logger.debug("Deny privilege request")
+            response.approval = False
         return response
 
-    def check_singleton(self):
-        ...
+    def _check_singleton(self) -> bool:
+        detected_nodes = self.get_node_names()
+        match = [
+            name
+            for name in detected_nodes
+            if name.startswith(self.NodeName) and (name != self.get_name())
+        ]
+        if len(match) > 0:
+            self.logger.error(
+                f"Authority server is already running ({match}), destroying this..."
+            )
+            self.destroy_node()
+        return True
 
-    def destroy_this(self, msg) -> NoReturn:
-        self.logger.error(f"Authority server is already running ({msg.identity})")
-        self.destroy_node()
+
+def main(args=None) -> None:
+    rclpy.init(args=args)
+    node = Authorizer()
+    try:
+        rclpy.spin(node)
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
