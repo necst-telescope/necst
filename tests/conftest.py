fff024e4412eca2372dac3d6f08250e671919ec2
KaoruNishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2022-09-05 01:36:26 +0000

    #14 Add deadlock checks

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,18 +1,32 @@
-import unittest
-
+import pytest
 import rclpy
+from rclpy.executors import SingleThreadedExecutor, MultiThreadedExecutor
 
 
-class TesterNode(unittest.TestCase):
+class TesterNode:
+    """Test class with rclpy configuration.
+
+    The implementation is inspired by the rclpy test suite.
+    https://github.com/ros2/rclpy/tree/rolling/rclpy/test
+
+    The ``unittest.TestCase`` implementations are replaced with PyTest's equivalents.
+    https://docs.pytest.org/en/6.2.x/xunit_setup.html#class-level-setup-teardown
+
+    """
 
     NodeName: str
 
     @classmethod
-    def setUpClass(cls) -> None:
+    def setup_class(cls) -> None:
         rclpy.init()
         cls.node = rclpy.create_node(cls.NodeName)
 
     @classmethod
-    def tearDownClass(cls) -> None:
+    def teardown_class(cls) -> None:
         cls.node.destroy_node()
         rclpy.shutdown()
+
+
+executor_type = pytest.mark.parametrize(
+    "executor_type", [SingleThreadedExecutor, MultiThreadedExecutor]
+)
