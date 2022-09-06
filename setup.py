68bb2860b8d0dc50bb9c4fe6ade3271950475ad7
r-yamada1998 <yamadier@icloud.com>
2022-09-06 08:24:12 +0000

    #4 modified setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,24 @@
 from setuptools import setup
 
-package_name = 'necst'
+package_name = "necst"
 
 setup(
     name=package_name,
-    version='0.1.0',
+    version="0.1.0",
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='Kaoru Nishikawa',
-    maintainer_email='k.nishikawa@a.phys.nagoya-u.ac.jp',
-    description='NEw Control System for Telescope',
-    license='MIT',
-    tests_require=['pytest'],
+    maintainer="Kaoru Nishikawa",
+    maintainer_email="k.nishikawa@a.phys.nagoya-u.ac.jp",
+    description="NEw Control System for Telescope",
+    license="MIT",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
+        "console_scripts": [
             f"pub={package_name}.tempos.ros2_string_pub:main",
             f"sub={package_name}.tempos.subscriber_member_function:main",
         ],
