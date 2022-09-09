1f982a09b5f7616223124a7242f1ffa9f538584a
MaoTamashiro <m.tamashiro@a.phys.nagoya-u.ac.jp>
2022-09-09 03:48:26 +0000

    #7 簡易コントローラ

---
+++ necst/core/commander.py
@@ -0,0 +1,74 @@
+from math import degrees
+from typing import Literal
+from rclpy.node import Node
+from necst_msgs.msg import CoordMsg, TimedAzElFloat64
+import necst
+import time
+# TimedAzElFloat64 は未実装 {az: float64, el: float64, time: float64}
+
+
+class Commander(Node):
+
+    node_name = "commander"
+
+    def __init__(self):
+        super().__init__(node_name)
+        self.publisher = {
+            "coord": self.create_publisher(CoordMsg, "raw_coord", 1),
+        }
+
+    def antenna(
+        self,
+        cmd: Literal["stop", "drive"],
+        *,
+        lon: float = None,
+        lat: float = None,
+        unit: str = None,
+        frame: str = None,
+        time: float = None,
+        name: str = None,
+        tracking_check: bool = True,
+    ) -> None:
+        if name != None:
+            msg = CoordMsg(time, name)
+            self.publisher["coord"].publish(msg)
+
+        else:
+            msg = CoordMsg(lon, lat, unit, frame, time)
+            self.publisher["coord"].publish(msg)
+
+        if tracking_check:
+            self.tracking_check("antenna")
+
+    def tracking_check(self, target: Literal["antenna"]) -> bool:
+        enc_az = enc_el = cmd_az = cmd_el = None
+        def enc_update(msg: CoordMsg) -> float:
+            nonlocal enc_az, enc_el
+            enc_az = msg.lon
+            enc_el = msg.lat
+        def cmd_update(msg: CoordMsg) -> float:
+            nonlocal cmd_az, cmd_el
+            cmd_az = msg.lon
+            cmd_el = msg.lat
+
+        subs_enc = self.create_subscription(CoordMsg, "encoder", enc_update)
+        subs_cmd = self.create_subscription(CoordMsg, "altaz", cmd_update)
+
+        Condition = Checker(enc_az, enc_el, cmd_az, cmd_el)
+
+        while Condition.Check():
+            time.sleep(0.05)
+
+        subs_enc.destroy()
+        subs_cmd.destroy()
+
+        return Condition.Check()
+
+class Checker:
+    def __init__(self, enc_az, enc_el, cmd_az, cmd_el) -> float:
+        self.condition = (cmd_az - enc_az)**2 + (cmd_el - enc_el)**2
+        self.threshold = necst.config.antenna_pointing_accuracy
+
+    def Check(self):
+        self.condition < self.threshold.to("deg").value
+
