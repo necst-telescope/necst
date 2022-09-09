ba99e0d2c77dabb186a85ef56c1a63a8f3f22c9c
KKnapier <107383192+KKnapier@users.noreply.github.com>
2022-09-09 14:29:23 +0900

    change class name and etc. 2nd

--- necst/core/controller/antenna.py
+++ necst/core/controller/antenna.py
@@ -5,19 +5,20 @@ from necst_msgs.msg import CoordMsg, PIDMsg, TimedAzElFloat64
 import time
 
 
-class config_PID(Node):
+class AntennaController(Node, Authorizer):
 
-    NodeName = "authorizer_pid"
-    Namespace = f"/necst/{config.observatory}/core/controller"
+    NodeName = "pid"
+    Namespace = f"/necst/{config.observatory}/core/pid"
 
     def __init__(self, frequency: float = 50):
+        super().__init__(frequency)
         self.controller = {
             "az": PIDController(),
             "el": PIDcontroller(),
         }
         self.create_subscription(CoordMsg, "altaz", self.init_ang, 1)
         self.create_subscription(CoordMsg, "encorder", self.init_enc, 1)
-        self.publisher = self.create_publisher(TimedAzElFloat64, "speed", int, 1)
+        self.publisher = self.create_publisher(TimedAzElFloat64, "speed", self.publisher, 1)
         self.create_timer(1/frequency, self.calc_pid)
         self.create_subscription_param(PIDMsg, "pid_param",
                                        self.change_pid_param, 1)
@@ -28,12 +29,12 @@ class config_PID(Node):
         msg = TimedAzElFloat64(az=az_speed, el=el_speed, time=time.time())
         self.publisher.publish(msg)
        
-    def init_ang(self, msg):
+    def get_ang(self, msg):
         self.az = msg.lon
         self.el = msg.lat
         self.t = msg.time
      
-    def init_enc(self, msg):
+    def get_enc(self, msg):
         self.az_enc = msg.lon
         self.el_enc = msg.lat
         self.t_enc = msg.time
@@ -47,7 +48,7 @@ class config_PID(Node):
 
 def main(args=None):
     rclpy.init(args=args)
-    node = config_PID()
+    node = AntennaController()
     rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
