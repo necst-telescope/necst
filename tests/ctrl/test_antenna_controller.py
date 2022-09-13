6a253eb2f6a00a21655efbf0c156cf64f0cbd661
KKnapier <107383192+KKnapier@users.noreply.github.com>
2022-09-13 16:10:33 +0900

    Update test_antenna_controller.py

--- tests/ctrl/test_antenna_controller.py
+++ tests/ctrl/test_antenna_controller.py
@@ -1,7 +1,7 @@
 import time
 from typing import Tuple
 
-from necst.ctrl import AntennaController
+import AntennaController
 from necst_msgs.msg import CoordMsg, PIDMsg, TimedAzElFloat64
 from ..conftest import TesterNode, is_destroyed, spinning
 
