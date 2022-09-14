c351d4de5b2edcdfa36e8dc65a51912a44a9de70
KaoruNishikawa <linersky246@gmail.com>
2022-09-14 02:24:32 +0000

    #28 Group relevant scripts

--- necst/ctrl/antenna/__init__.py
+++ necst/ctrl/antenna/__init__.py
@@ -1,1 +1,2 @@
 from .controller import AntennaController  # noqa: F401
+from .coordinate_conversion import *  # noqa: F401,F403
--- necst/ctrl/calculations/__init__.py
+++
@@ -1,1 +0,0 @@
-from .coordinate_conversion import *  # noqa: F401, F403
