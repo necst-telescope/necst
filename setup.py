3bacf088a0234a4511709a6acfe3a8b6d53737d5
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2022-09-15 10:43:37 +0000

    #28 Automate entrypoint definition

--- setup.py
+++ setup.py
@@ -1,6 +1,32 @@
+from pathlib import Path
 from setuptools import setup, find_packages
+from typing import List
 
 package_name = "necst"
+
+
+def get_executor_entrypoints() -> List[str]:
+    """Get entrypoint expression for every executor-definition files found.
+
+    This function should never be executed in ``setuptools.setup``, as the function may
+    be executed in different directory, causing incorrect ``__file__``.
+
+    """
+    root = Path(__file__).parent / package_name
+    exec_files = root.glob("**/exec_*.py")
+
+    def _parse(path: Path) -> str:
+        cmd = path.stem.strip("exec_")
+        path_with_no_extension = path.with_suffix("")
+        dot_separated_path = str(path_with_no_extension).replace("/", ".")
+        return f"{cmd}={package_name}.{dot_separated_path}:main"
+
+    paths_in_pkg = [f.relative_to(root) for f in exec_files]
+    return [_parse(p) for p in paths_in_pkg]
+
+
+executor_entrypoints = get_executor_entrypoints()
+
 
 setup(
     name=package_name,
@@ -18,9 +44,9 @@ setup(
     license="MIT",
     tests_require=["pytest"],
     entry_points={
-        "console_scripts": [
-            f"pub={package_name}.tempos.ros2_string_pub:main",
-            f"sub={package_name}.tempos.subscriber_member_function:main",
+        "console_scripts": executor_entrypoints
+        + [
+            # Custom paths will be added here
         ],
     },
 )
