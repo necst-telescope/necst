9d2e5c949fbd20505dc24b2452fa8df359eb147e
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2022-09-16 00:30:08 +0000

    #28 Add namespace definitions

--- necst/__init__.py
+++ necst/__init__.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from importlib.metadata import version
 
 import neclib
@@ -9,3 +10,14 @@ except:  # noqa: E722
     __version__ = "0.0.0"
 
 config = neclib.config
+
+
+@dataclass
+class namespace:
+    root: str = f"/necst/{config.observatory}"
+
+    ctrl: str = f"{root}/ctrl"
+    antenna: str = f"{ctrl}/antenna"
+
+    core: str = f"{root}/core"
+    auth: str = f"{core}/auth"
