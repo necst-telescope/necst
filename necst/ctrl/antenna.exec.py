5ee54bc2213a1d90accdb7ed12dc51a67928da8c
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2022-09-16 02:04:58 +0000

    #28 Use high-definition simulator

--- necst/ctrl/antenna.exec.py
+++ necst/ctrl/antenna.exec.py
@@ -1,45 +1,32 @@
 import rclpy
 from rclpy.executors import MultiThreadedExecutor
 
-from .antenna import AntennaController
+from .antenna import AntennaPIDController, AntennaDriver, HorizontalCoord
 
 
-from rclpy.node import Node
-from necst import config
-from necst_msgs.msg import CoordMsg
-
-
-class AntennaSimulator(Node):
-    Namespace = f"/necst/{config.observatory}/ctrl/antenna"
-    NodeName = "antenna_simulator"
-
-    def __init__(self) -> None:
-        super().__init__(self.NodeName, namespace=self.Namespace)
-        self.pub = self.create_publisher(CoordMsg, "encoder", 1)
-        self.create_subscription(CoordMsg, "altaz", self.respond, 1)
-
-    def respond(self, msg: CoordMsg) -> None:
-        assert msg.frame == "altaz"
-        assert msg.unit == "deg"
-        self.pub.publish(msg)
+def configure_executor() -> MultiThreadedExecutor:
+    executor = MultiThreadedExecutor()
+    nodes = [
+        AntennaPIDController(),
+        HorizontalCoord(),
+        AntennaDriver(),
+    ]
+    _ = [executor.add_node(n) for n in nodes]
+    return executor
 
 
 def main(args=None) -> None:
     rclpy.init(args=args)
 
-    executor = MultiThreadedExecutor()
-    nodes = [
-        AntennaController(),
-        AntennaSimulator(),  # TODO: Replace with high-definition simulator
-    ]
-    _ = [executor.add_node(n) for n in nodes]
+    executor = configure_executor()
+
     try:
         executor.spin()
     except KeyboardInterrupt:
         pass
     finally:
         executor.shutdown()
-        _ = [n.destroy_node() for n in nodes]
+        _ = [n.destroy_node() for n in executor.get_nodes()]
         rclpy.try_shutdown()
 
 
