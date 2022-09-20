1040e68fb97290cddd0cbe9f4b1d3a54fe094e0a
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2022-09-20 12:53:25 +0000

    #33 Reflect module name change

--- necst/ctrl/antenna/__init__.py
+++ necst/ctrl/antenna/__init__.py
@@ -1,3 +1,3 @@
 from .pid_controller import AntennaPIDController  # noqa: F401
-from .coordinate_conversion import *  # noqa: F401,F403
+from .horizontal_coord import *  # noqa: F401,F403
 from .driver import AntennaDriver  # noqa: F401
