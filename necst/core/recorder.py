615c9ee802809d962f76c2b95bc6e9741ba086d4
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2022-09-29 08:16:04 +0000

    #50 Fix DBWriter usage

--- necst/core/recorder.py
+++ necst/core/recorder.py
@@ -1,4 +1,5 @@
 import importlib
+from functools import partial
 from typing import Any
 
 from neclib.recorders import DBWriter
@@ -12,15 +13,17 @@ class Recorder(Node):
     NodeName = "recorder"
     Namespace = namespace.core
 
+    TopicScanInterval: float = 1.0
+
     def __init__(self) -> None:
         super().__init__(self.NodeName, namespace=self.Namespace)
 
-        self.recoder = DBWriter(config.record_root)
+        self.recorder = DBWriter(config.record_root)
 
         self.subscriber = {}
 
-        self.create_timer(3, self.scan_topics)
-        self.recoder.start_recording()
+        self.create_timer(self.TopicScanInterval, self.scan_topics)
+        self.recorder.start_recording()
 
     def _get_msg_type(self, path: str) -> Any:
         module_name, msg_name = path.replace("/", ".").rsplit(".", 1)
@@ -38,20 +41,36 @@ class Recorder(Node):
 
     def scan_topics(self):
         topics = self.get_topic_names_and_types()
-        for name, msg_type in topics:
-            msg_type = msg_type[0]
-            table_name = name.replace("/", "-").strip("-")
-            if table_name not in self.recoder.tables.keys():
-                header = {"data": self._get_msg_field_info(msg_type)}
-                self.get_logger().warning(str(header))
-                self.recoder.add_table(table_name, header)
+        for name, msg_type_str in topics:
+            (msg_type_str,) = msg_type_str  # Extract only element of list
+            if name not in self.subscriber.keys():
+                msg_type = self._get_msg_type(msg_type_str)
+                self.subscriber[name] = self.create_subscription(
+                    msg_type, name, partial(self.append, topic_name=name), qos.lowest
+                )
+                # Callback argument isn't supported in `create_subscription`.
+                # The following link can provide a solution, i.e.,
+                # `callback=lambda msg: self.append(msg, name)` but argument `name` is
+                # just a reference, so the common callback can be called with unexpected
+                # value.
+                # https://answers.ros.org/question/362954/ros2create_subscription-how-to-pass-callback-arguments/?answer=393430#post-id-393430
 
-                msg = self._get_msg_type(msg_type)
-                self.create_subscription(msg, name, self.append, qos.lowest)
+    def append(self, msg, topic_name: str) -> None:
+        fields = msg.get_fields_and_field_types()
+        chunk = [
+            {"key": name, "type": type_, "value": getattr(msg, name)}
+            for name, type_ in fields.items()
+        ]
+        self.recorder.append(topic_name, chunk)
 
-    def append(self, msg) -> None:
-        info = msg.get_fields_and_field_types()
-        self.recoder.append({k: getattr(msg, k) for k, _ in info})
+    def destroy_node(self) -> None:
+        try:
+            # IMPORTANT: If `stop_recording` isn't called, no data will be saved.
+            self.recorder.stop_recording()
+        except AttributeError:
+            pass
+        finally:
+            super().destroy_node()
 
 
 def main(args=None):
