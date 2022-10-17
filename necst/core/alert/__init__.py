e4ebef870c12ef4f250367a2220f3a5a86a26863
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2022-10-17 01:32:47 +0000

    #46 Add description on alert package

--- necst/core/alert/__init__.py
+++ necst/core/alert/__init__.py
@@ -1,3 +1,15 @@
+"""Check system status, and invoke safe guards if needed.
+
+Multiple safeguards (subclasses of ``Guard``) can be attached to centralized alert
+handler ``AlertHandler``.
+
+Notes
+-----
+This package should have high-extensibility, as status to watch differs between
+telescopes.
+
+"""
+
 from .alert_handler import AlertHandler  # noqa: F401
 
 from .guard_base import Guard  # noqa: F401
