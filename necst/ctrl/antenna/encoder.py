b69506bc4523842c713c6e681a466173f27de7a0
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2022-10-25 07:13:01 +0000

    Fix parameter usage

--- necst/ctrl/antenna/encoder.py
+++ necst/ctrl/antenna/encoder.py
@@ -16,13 +16,13 @@ class AntennaEncoder(Node):
         super().__init__(self.NodeName, namespace=self.Namespace)
         self.publisher = self.create_publisher(CoordMsg, "encoder", qos.realtime)
         ports = config.antenna_nd287_port
-        self.encoder_az = antenna_encoder(ports.az)
-        self.encoder_el = antenna_encoder(ports.el)
+        self.encoder = antenna_encoder()#ports.az)
+        #self.encoder_el = antenna_encoder()#ports.el)
         self.create_timer(1 / config.antenna_command_frequency, self.stream)
 
     def stream(self) -> None:
-        az_reading = self.encoder_az.get_reading()
-        el_reading = self.encoder_el.get_reading()
+        az_reading = self.encoder.get_reading("az")
+        el_reading = self.encoder.get_reading("el")
         msg = CoordMsg(
             lon=az_reading, lat=el_reading, unit="deg", frame="altaz", time=time.time()
         )
