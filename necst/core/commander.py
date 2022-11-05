28a613f78167c8be233bff783360086601e6c318
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2022-11-05 08:00:37 +0000

    #49 Refactor topic handling

--- necst/core/commander.py
+++ necst/core/commander.py
@@ -1,9 +1,11 @@
 import time as pytime
-from typing import Literal
+from collections import defaultdict
+from functools import partial
+from typing import Any, Literal
 
 from neclib.utils import ConditionChecker
 
-from .. import config, namespace, qos
+from .. import config, namespace, qos, utils
 from .auth import PrivilegedNode, require_privilege
 from necst_msgs.msg import CoordMsg
 
@@ -20,6 +22,26 @@ class Commander(PrivilegedNode):
                 CoordMsg, f"{namespace.antenna}/raw_coord", qos.reliable
             ),
         }
+        self.subscription = {
+            "encoder": self.create_subscription(
+                CoordMsg,
+                f"{namespace.antenna}/encoder",
+                partial(self.__callback, "encoder"),
+                qos.realtime,
+            ),
+            "altaz": self.create_subscription(
+                CoordMsg,
+                f"{namespace.antenna}/altaz",
+                partial(self.__callback, "altaz"),
+                qos.realtime,
+            ),
+        }
+
+        self.parameters = defaultdict(lambda: None)
+
+    def __callback(self, name: str, msg: Any):
+        self.publisher["coord"].publish(CoordMsg(name=name))
+        self.parameters[name] = msg
 
     @require_privilege
     def antenna(
@@ -35,32 +57,42 @@ class Commander(PrivilegedNode):
         wait: bool = True,
     ) -> None:
         if cmd.lower() == "stop":
-            # TODO: Consider using alert.
-            recv = False
+            # with utils.spinning(self):
+            while True:
+                if self.parameters["speed"] is None:
+                    # self.publisher["coord"].publish(CoordMsg(name="continue"))
+                    pytime.sleep(0.05)
+                    continue
+                speed = self.parameters["speed"]
+                if (abs(speed.az) < 1e-3) and (abs(speed.el) < 1e-3):
+                    break
+                self.publisher["coord"].publish(self.parameters["encoder"])
+                pytime.sleep(config.antenna_command_interval_sec)
+            return
 
-            def send_cmd(msg: CoordMsg) -> None:
-                nonlocal recv
-                self.publisher["coord"].publish(msg)
-                recv = True
-
-            subs_enc = self.create_subscription(
-                CoordMsg, f"{namespace.antenna}/encoder", send_cmd, qos.realtime
-            )
-            while not recv:
-                pytime.sleep(0.02)
-            self.destroy_subscription(subs_enc)
         elif cmd.lower() == "point":
             if name is not None:
                 msg = CoordMsg(time=time, name=name)
-                self.publisher["coord"].publish(msg)
             else:
                 msg = CoordMsg(
                     lon=float(lon), lat=float(lat), unit=unit, frame=frame, time=time
                 )
-                self.publisher["coord"].publish(msg)
+            self.publisher["coord"].publish(msg)
 
             if wait:
                 self.tracking_check("antenna")
+            return
+
+        else:
+            raise NotImplementedError(f"Command '{cmd}' isn't implemented yet.")
+
+    @require_privilege
+    def chopper(self, cmd: Literal["insert", "eject"]):
+        """Calibrator."""
+        if cmd.lower() == "insert":
+            ...
+        elif cmd.lower() == "eject":
+            ...
         else:
             raise NotImplementedError(f"Command '{cmd}' isn't implemented yet.")
 
@@ -71,44 +103,29 @@ class Commander(PrivilegedNode):
     def tracking_check(
         self, target: Literal["antenna", "dome"], timeout_sec: float = None
     ) -> bool:
-        # TODO: Implement as independent node.
-        topic_name = {
-            "antenna": [f"{namespace.antenna}/encoder", f"{namespace.antenna}/altaz"],
+        param_name = {
+            "antenna": ["encoder", "altaz"],
             "dome": ["", ""],  # TODO: Implement.
         }
         threshold = {
             "antenna": config.antenna_pointing_accuracy.to_value("deg"),
             "dome": ...,
         }
-        enc_az = enc_el = cmd_az = cmd_el = None
-
-        def enc_update(msg: CoordMsg) -> float:
-            nonlocal enc_az, enc_el
-            enc_az, enc_el = msg.lon, msg.lat
-
-        def cmd_update(msg: CoordMsg) -> float:
-            nonlocal cmd_az, cmd_el
-            cmd_az, cmd_el = msg.lon, msg.lat
-
-        subs_enc = self.create_subscription(
-            CoordMsg, topic_name[target][0], enc_update, qos.realtime
-        )
-        subs_cmd = self.create_subscription(
-            CoordMsg, topic_name[target][1], cmd_update, qos.realtime
-        )
+        ENC, CMD = param_name[target]
 
         timelimit = None if timeout_sec is None else pytime.time() + timeout_sec
         checker = ConditionChecker(10, reset_on_failure=True)
-        while True:
-            if any(p is None for p in [enc_az, enc_el, cmd_az, cmd_el]):
+        with utils.spinning(self):
+            while True:
+                if (self.params[ENC] is None) or (self.params[CMD] is None):
+                    pytime.sleep(0.05)
+                    continue
+                error_az = self.params[ENC].az - self.params[CMD].az
+                error_el = self.params[ENC].el - self.params[CMD].el
+                if checker.check(
+                    error_az**2 + error_el**2 < threshold[target] ** 2
+                ):
+                    return True
+                if (timelimit is not None) and (pytime.time() > timelimit):
+                    return False
                 pytime.sleep(0.05)
-                continue
-            error_az = enc_az - cmd_az
-            error_el = enc_el - cmd_el
-            if checker.check(error_az**2 + error_el**2 < threshold[target] ** 2):
-                self.destroy_subscription(subs_enc)
-                self.destroy_subscription(subs_cmd)
-                return True
-            if (timelimit is not None) and (pytime.time() > timelimit):
-                return False
-            pytime.sleep(0.05)
