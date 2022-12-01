f57ffb75e4974417f68cfff0e4dab841f370081b
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2022-12-01 08:17:35 +0000

    #110 Fix type hints

--- necst/core/commander.py
+++ necst/core/commander.py
@@ -1,7 +1,7 @@
 import time as pytime
 from collections import defaultdict
 from functools import partial
-from typing import Any, Literal, Optional
+from typing import Any, Literal, Optional, Union
 
 from neclib.utils import ConditionChecker
 from necst_msgs.msg import AlertMsg, ChopperMsg, CoordMsg, PIDMsg
@@ -15,7 +15,7 @@ class Commander(PrivilegedNode):
     NodeName = "commander"
     Namespace = namespace.core
 
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__(self.NodeName, namespace=self.Namespace)
         self.publisher = {
             "coord": topic.raw_coord.publisher(self),
@@ -38,12 +38,12 @@ class Commander(PrivilegedNode):
             ),
         }
 
-        self.parameters = defaultdict(lambda: None)
+        self.parameters = defaultdict[str, Optional[Any]](lambda: None)
 
-    def __callback(self, name: str, msg: Any):
+    def __callback(self, name: str, msg: Any) -> None:
         self.parameters[name] = msg
 
-    def __get_parameter(self, key: str) -> Any:
+    def get_parameter(self, key: str) -> Any:
         while self.parameters[key] is None:
             pytime.sleep(0.01)
         return self.parameters[key]
@@ -53,17 +53,17 @@ class Commander(PrivilegedNode):
         self,
         cmd: Literal["stop", "point", "scan", "?"],
         *,
-        lon: Optional[float] = None,
-        lat: Optional[float] = None,
+        lon: Optional[Union[int, float]] = None,
+        lat: Optional[Union[int, float]] = None,
         unit: Optional[str] = None,
         frame: Optional[str] = None,
-        time: Optional[float] = 0.0,
+        time: Optional[Union[int, float]] = 0,
         name: Optional[str] = None,
         wait: bool = True,
     ) -> None:
         """Control antenna direction and motion."""
-        cmd = cmd.upper()
-        if cmd == "STOP":
+        CMD = cmd.upper()
+        if CMD == "STOP":
             target = [namespace.antenna]
             msg = AlertMsg(critical=True, warning=True, target=target)
             checker = ConditionChecker(5, reset_on_failure=True)
@@ -79,7 +79,7 @@ class Commander(PrivilegedNode):
             self.publisher["alert_stop"].publish(msg)
             return
 
-        elif cmd == "POINT":
+        elif CMD == "POINT":
             if name is not None:
                 msg = CoordMsg(time=time, name=name)
             else:
@@ -95,36 +95,39 @@ class Commander(PrivilegedNode):
     @require_privilege(escape_cmd=["?"])
     def chopper(self, cmd: Literal["insert", "remove", "?"], wait: bool = True):
         """Calibrator."""
-        cmd = cmd.upper()
-        if cmd == "?":
-            return self.__get_parameter("chopper")
-        elif cmd == "INSERT":
+        CMD = cmd.upper()
+        if CMD == "?":
+            return self.get_parameter("chopper")
+        elif CMD == "INSERT":
             msg = ChopperMsg(insert=True, time=pytime.time())
             self.publisher["chopper"].publish(msg)
-        elif cmd == "REMOVE":
+        elif CMD == "REMOVE":
             msg = ChopperMsg(insert=False, time=pytime.time())
             self.publisher["chopper"].publish(msg)
         else:
             raise ValueError(f"Unknown command: {cmd!r}")
 
         if wait:
-            target_status = cmd == "INSERT"
-            while self.__get_parameter("chopper").insert is not target_status:
+            target_status = CMD == "INSERT"
+            while self.get_parameter("chopper").insert is not target_status:
                 pytime.sleep(0.1)
 
     def wait_convergence(
-        self, target: Literal["antenna", "dome"], timeout_sec: Optional[float] = None
+        self,
+        target: Literal["antenna", "dome"],
+        timeout_sec: Optional[Union[int, float]] = None,
     ) -> None:
         """Wait until the motion has been converged."""
-        param_name = {
+        _param_name = {
             "antenna": ["encoder", "altaz"],
             "dome": ["", ""],  # TODO: Implement.
         }
-        threshold = {
+        _threshold = {
             "antenna": config.antenna_pointing_accuracy.to_value("deg"),
             "dome": ...,
         }
-        ENC, CMD = param_name[target.lower()]
+        ENC, CMD = _param_name[target.lower()]
+        threshold = _threshold[target.lower()]
 
         timelimit = None if timeout_sec is None else pytime.time() + timeout_sec
         checker = ConditionChecker(10, reset_on_failure=True)
@@ -134,14 +137,18 @@ class Commander(PrivilegedNode):
                 continue
             error_az = self.parameters[ENC].lon - self.parameters[CMD].lon
             error_el = self.parameters[ENC].lat - self.parameters[CMD].lat
-            if checker.check(error_az**2 + error_el**2 < threshold[target] ** 2):
+            if checker.check(error_az**2 + error_el**2 < threshold**2):
                 return
             pytime.sleep(0.05)
         raise NECSTTimeoutError("Couldn't confirm drive convergence")
 
     @require_privilege
     def pid_parameter(
-        self, Kp: float, Ki: float, Kd: float, axis: Literal["az", "el"]
+        self,
+        Kp: Union[int, float],
+        Ki: Union[int, float],
+        Kd: Union[int, float],
+        axis: Literal["az", "el"],
     ) -> None:
         """Change PID parameters."""
         if axis.lower() not in ("az", "el"):
