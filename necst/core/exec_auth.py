1baa5e6ddbf9d319c2da47621013b6411ddcdf89
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2022-12-01 22:42:02 +0000

    #113 Fix usage of authorizer

--- necst/core/exec_auth.py
+++ necst/core/exec_auth.py
@@ -1,28 +1,26 @@
 import rclpy
-from rclpy.executors import SingleThreadedExecutor
+from rclpy.node import Node
 
 from . import Authorizer
 
 
-def configure_executor() -> SingleThreadedExecutor:
-    executor = SingleThreadedExecutor()
-    nodes = [
-        Authorizer(),
-    ]
-    _ = [executor.add_node(n) for n in nodes]
-    return executor
+def configure_server() -> Node:
+    node = Authorizer()
+    return node
 
 
 def main(args=None) -> None:
     rclpy.init(args=args)
 
-    executor = configure_executor()
+    node = configure_server()
+    executor = rclpy.get_global_executor()
 
     try:
         executor.spin()
     except KeyboardInterrupt:
         pass
     finally:
+        node.destroy_node()
         executor.shutdown()
         _ = [n.destroy_node() for n in executor.get_nodes()]
         rclpy.try_shutdown()
