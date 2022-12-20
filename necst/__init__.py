933f06254baf7ea1ae16989ade03245cc75cff97
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2022-12-20 12:08:10 +0000

    #143 Make exception details available when environment variable is set

--- necst/__init__.py
+++ necst/__init__.py
@@ -1,6 +1,7 @@
 """NEw Control System for Telescope.
 """
 
+import os
 from importlib.metadata import version
 
 import neclib
@@ -17,3 +18,18 @@ config = neclib.config
 from neclib.exceptions import *  # noqa: E402, F401, F403
 
 from .definitions import *  # noqa: E402, F401, F403
+
+if os.environ.get("NECST_DEBUG_MODE", None):
+    import sys
+    import time
+    import traceback
+
+    from rclpy.task import Future
+
+    def __del__(self: Future):
+        if self._exception is not None and not self._exception_fetched:
+            exc = traceback.format_exception(self._exception)
+            exc.insert(0, f"[{time.time()}]\n")
+            print("".join(exc), file=sys.stderr)
+
+    Future.__del__ = __del__
