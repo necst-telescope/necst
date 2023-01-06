6975a94db88a1475a3943e7717ff4d7a0ab4f75b
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2023-01-06 03:20:10 +0000

    #140 Auto-save parameter files

--- necst/observation/observation_base.py
+++ necst/observation/observation_base.py
@@ -1,7 +1,9 @@
+import os
 import time
 from abc import ABC, abstractmethod
 from contextlib import contextmanager
 from datetime import datetime
+from pathlib import Path
 from typing import Any, Generator, Optional, Union, final
 
 import rclpy
@@ -15,6 +17,8 @@ class Observation(ABC):
     observation_type: str
     target: Optional[str] = None
 
+    parameter_files = ("config.toml", "pointing_param.toml")
+
     def __init__(self, *args, **kwargs) -> None:
         self.logger = get_logger(self.__class__.__name__)
         self.execute(*args, **kwargs)
@@ -26,6 +30,7 @@ class Observation(ABC):
             try:
                 self.com.metadata("set", position="", id="")
                 self.com.record("start", name=self.record_name)
+                self.record_parameter_files()
                 self.run(*args, **kwargs)
             finally:
                 self.com.record("stop")
@@ -49,6 +54,15 @@ class Observation(ABC):
         now = datetime.utcnow().strftime("%Y%m%d_%H%M%S_")
         target = "" if self.target is None else f"_{self.target}"
         return f"necst_{now}{self.observation_type}{target}"
+
+    def record_parameter_files(self) -> None:
+        root = os.environ.get("NECST_ROOT", Path.home() / ".necst")
+        for filename in self.parameter_files:
+            try:
+                path = f"{root.rstrip('/')}/{filename.lstrip('/')}"
+                self.com.record("file", name=path)
+            except FileNotFoundError:
+                self.logger.error(f"Failed to save parameter file {filename!r}")
 
     @abstractmethod
     def run(self, *args, **kwargs) -> None:
