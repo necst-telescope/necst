2c471bd039e17972e08a22701d3579426766e66f
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2023-01-11 11:57:11 +0000

    #152 Add console script for OTF observation

---
+++ bin/otf.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""On-The-Fly observation.
+
+All parameters for the observation should be given as a TOML format parameter file.
+
+Examples
+--------
+$ necst otf -f "otf_orikl.toml"
+
+"""
+
+import argparse
+
+from necst.procedures import OTF
+
+if __name__ == "__main__":
+    description = "On-The-Fly Observation"
+    p = argparse.ArgumentParser(description=description)
+    p.add_argument(
+        "-f",
+        "--file",
+        type=str,
+        help="Path to observation parameter file",
+        required=True,
+    )
+    args = p.parse_args()
+
+    OTF(path=args.file)
