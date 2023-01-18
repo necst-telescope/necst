f0c1e7dcd5a2c80ddf7c7fd5c68ce6ae928ec490
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2023-01-18 13:02:08 +0000

    #152 Use observation spec file

--- bin/radio_pointing.py
+++ bin/radio_pointing.py
@@ -6,7 +6,7 @@ specified by its coordinate.
 
 Examples
 --------
-$ radio_pointing -t "IRC+10216"
+$ radio_pointing -f  "pt_orikl.toml"
 
 """
 
@@ -18,47 +18,12 @@ if __name__ == "__main__":
     description = "Radio Pointing Observation"
     p = argparse.ArgumentParser(description=description)
     p.add_argument(
-        "-n",
-        type=int,
-        help="Number of repetitions",
-        default=-1,
-    )
-    p.add_argument(
-        "-i",
-        "--integ",
-        type=float,
-        help="Integration time for the R-sky obs.",
-        default=2,
-    )
-    p.add_argument("--speed", type=float, help="Scan speed in [deg/s].", default=1 / 30)
-    p.add_argument(
-        "-s",
-        "--separation",
-        type=float,
-        help="Separation between observing points for PSW mode. Separation from target "
-        "to start/stop point for scan mode.",
-        default=1 / 60,
-    )
-    p.add_argument(
-        "-p",
-        "--points",
-        type=int,
-        help="Number of points to observe. If 0 or negative value specified, scan "
-        "observation is performed.",
-        default=9,
-    )
-    p.add_argument(
-        "-t", "--target", type=str, help="Name of target celestial body.", required=True
+        "-f",
+        "--file",
+        type=str,
+        help="Path to observation parameter file",
+        required=True,
     )
     args = p.parse_args()
 
-    RadioPointing(
-        n_iter=args.n,
-        integ_time=args.integ,
-        unit="deg",
-        speed=args.speed,
-        separation=args.separation,
-        offset_frame="altaz",
-        method=args.points,
-        target=args.target,
-    )
+    RadioPointing(path=args.file)
--- necst/procedures/observations/radio_pointing.py
+++ necst/procedures/observations/radio_pointing.py
@@ -1,5 +1,8 @@
+import os
 from itertools import count
 from typing import Any, Generator, Tuple, Union
+
+from neclib.parameters import ObservationSpec
 
 from .observation_base import Observation
 
@@ -8,92 +11,109 @@ class RadioPointing(Observation):
 
     observation_type = "RadioPointing"
 
-    def run(
-        self,
-        target: Union[str, Tuple[float, float, str]],
-        method: int = 9,
-        offset_frame: str = "altaz",
-        separation: float = 1 / 60,
-        speed: float = 1 / 30,
-        unit: str = "deg",
-        n_iter: int = 0,
-        integ_time: Union[int, float] = 5,
-    ) -> None:
-        if (method > 0) and ((method - 1) % 4 != 0):
+    def run(self, path: Union[str, os.PathLike]) -> None:
+        p = ObservationSpec.from_file(path)
+        self.com.record("file", name=path)
+
+        if (p.method > 0) and ((p.method - 1) % 4 != 0):
             raise ValueError("Invalid pointing method")
 
-        antenna_point_kwargs = dict(wait=True, unit=unit)
         antenna_scan_kwargs = dict(
-            scan_frame=offset_frame, unit=unit, speed=speed, wait=True
+            scan_frame=p.delta_coord, unit="deg", speed=p.speed, wait=True
         )
-        if isinstance(target, str):
-            antenna_point_kwargs.update(name=target)
-            antenna_scan_kwargs.update(name=target)
+        if any(p[x] is None for x in ["lambda_on", "beta_on"]):
+            antenna_scan_kwargs.update(name=p.target)
         else:
-            antenna_point_kwargs.update(target=target)
-            antenna_scan_kwargs.update(reference=target)
+            on = (p.lambda_on.to_value("deg"), p.beta_on.to_value("deg"), p.coord_sys)
+            antenna_scan_kwargs.update(reference=on)
 
-        self.com.antenna("point", **antenna_point_kwargs)
-        self.hot(integ_time, -1)
+        self.track_off_point(p)
+        self.hot(p.integ_hot, -1)
+        self.off(p.integ_off, id=-1)
+        # TODO: Run every time interval is met
 
-        iterate_counter = range(n_iter) if n_iter > 0 else count()
+        iterate_counter = range(p.n) if p.n > 0 else count()
 
-        if method > 0:
+        if p.method > 0:
             for idx in iterate_counter:
                 self.logger.info(f"Starting {idx}th sequence")
-                self.discrete(
-                    idx,
-                    method,
-                    separation,
-                    offset_frame,
-                    integ_time,
-                    antenna_point_kwargs,
-                )
+                self.discrete(idx, p)
         else:
+            sep = (
+                p.max_separation_az.to_value("deg"),
+                p.max_separation_el.to_value("deg"),
+            )
             for idx in iterate_counter:
-                self.scan(idx, separation, antenna_scan_kwargs)
+                self.scan(idx, sep, antenna_scan_kwargs)
 
-        self.hot(integ_time, 9999)
+        self.hot(p.integ_hot.to_value("s"), 9999)
 
     def discrete(
         self,
         id: Any,
-        method: int,
-        separation: float,
-        offset_frame: str,
-        integ_time: Union[int, float],
-        antenna_point_kwargs,
+        spec: ObservationSpec,
     ) -> None:
-        offsets = self.get_offset(method, separation)
+        separation = (spec.grid_az.to_value("deg"), spec.grid_el.to_value("deg"))
+        offsets = self.get_offset(spec.method, separation)
         for i, (offset_lon, offset_lat) in enumerate(offsets):
             self.logger.info(f"Starting integration at {i}th point")
-            self.com.antenna(
-                "point",
-                offset=(offset_lon, offset_lat, offset_frame),
-                **antenna_point_kwargs,
-            )
-            self.sky(integ_time, f"{id}-{i}")
+            self.track_on_point(spec, offset=(offset_lon, offset_lat, spec.delta_coord))
+            self.sky(spec.integ_on.to_value("s"), f"{id}-{i}")
 
-    def scan(self, id: Any, width: float, antenna_scan_kwargs) -> None:
+    def scan(self, id: Any, width: Tuple[float, float], antenna_scan_kwargs) -> None:
+        width_x, width_y = width
+
         self.logger.info("Starting X-scan")
         self.com.metadata("set", position="SKY", id=f"{id}-x", intercept=False)
         self.com.antenna(
-            "scan", start=(-1 * width, 0), stop=(width, 0), **antenna_scan_kwargs
+            "scan", start=(-1 * width_x, 0), stop=(width_x, 0), **antenna_scan_kwargs
         )
         self.com.metadata("set", position="", id="")
 
         self.logger.info("Starting Y-scan")
         self.com.metadata("set", position="SKY", id=f"{id}-y", intercept=False)
         self.com.antenna(
-            "scan", start=(0, -1 * width), stop=(0, width), **antenna_scan_kwargs
+            "scan", start=(0, -1 * width_y), stop=(0, width_y), **antenna_scan_kwargs
         )
         self.com.metadata("set", position="", id="")
 
     def get_offset(
-        self, method: int, separation: float
+        self, method: int, separation: Tuple[float, float]
     ) -> Generator[Tuple[float, float], None, None]:
+        separation_x, separation_y = separation
         n_per_arm = int((method - 1) / 4)
         for offset in range(-n_per_arm, n_per_arm + 1):
-            yield (offset * separation, 0.0)
+            yield (offset * separation_x, 0.0)
         for offset in range(-n_per_arm, n_per_arm + 1):
-            yield (0.0, offset * separation)
+            yield (0.0, offset * separation_y)
+
+    def track_off_point(self, spec: ObservationSpec) -> None:
+        if spec.relateve:
+            offset = (
+                spec.delta_lambda.to_value("deg"),
+                spec.delta_beta.to_value("deg"),
+                spec.delta_coord,
+            )
+            self.track_on_point(spec, offset=offset)
+        off = (
+            spec.lambda_off.to_value("deg"),
+            spec.beta_off.to_value("deg"),
+            spec.coord_sys,
+        )
+        kwargs = dict(wait=True, unit="deg", target=off)
+        self.com.antenna("point", **kwargs)
+
+    def track_on_point(
+        self, spec: ObservationSpec, offset: Tuple[float, float, str]
+    ) -> None:
+        kwargs = dict(wait=True, unit="deg")
+        if any(spec[x] is None for x in ["lambda_on", "beta_on"]):
+            kwargs.update(name=spec.target)
+        else:
+            on = (
+                spec.lambda_on.to_value("deg"),
+                spec.beta_on.to_value("deg"),
+                spec.coord_sys,
+            )
+            kwargs.update(target=on)
+        self.com.antenna("point", offset=offset, **kwargs)
