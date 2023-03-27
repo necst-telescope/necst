5ad4f90e2e79e474eaf5eb08327b5b72519b18b0
Yamato Matsuzuki <y.matsuzuki@a.phys.nagoya-u.ac.jp>
2023-03-27 08:04:43 +0000

    #166 Use commander for voltage/current control

--- necst/procedures/measurement/sis_iv.py
+++ necst/procedures/measurement/sis_iv.py
@@ -1,38 +1,25 @@
-import rclpy
 import time
 
 from ..observation_base import Observation
-from necst_msgs.msg import SISIV as SISIVMsg
-from neclib.devcices import ADConverter, DAConverter
 
 
 class SIS_IV(Observation):
 
     observation_type = "SISIV"
 
-    def __init__(self) -> None:
-        self.publisher_ = self.create_publisher(SISIVMsg, 'SIS_IVdata', 10)
-        self.da_converter = DAConverter()
-        self.ad_converter = ADConverter()
-
-    def run(self, sideband: str, min_voltage_mV: float = -8.0, max_voltage_mV: float = 8.0, step_voltage_mV: float = 0.1) -> None:
-        msg = SISIVMsg()
-        channels = self.ad_converter.Config.get("bias_setter", "channel")
-        if sideband == "USB":
-            channel = channels["USB"]
-        elif sideband == "LSB":
-            channel = channels["LSB"]
-        else:
-            raise ValueError("sideband must be either 'USB' or 'LSB'.")
-
-        for bias_voltage in range(int(round(1000 * min_voltage_mV)), int(round(1000 * max_voltage_mV) + 1000 * step_voltage_mV), int(round(1000 * step_voltage_mV))):
-            setting_voltage = self.da_converter.set_voltage(bias_voltage / 1000, channel)
+    def run(
+        self,
+        id: str,
+        min_voltage_mV: float = -8.0,
+        max_voltage_mV: float = 8.0,
+        step_voltage_mV: float = 0.1,
+    ) -> None:
+        for bias_voltage in range(
+            int(round(1000 * min_voltage_mV)),
+            int(round(1000 * max_voltage_mV) + 1000 * step_voltage_mV),
+            int(round(1000 * step_voltage_mV)),
+        ):
+            self.com.sis_bias("set", bias_voltage / 1000, id=id)
             time.sleep(0.1)
-            measured_voltage = self.ad_converter.get_voltage(channel).to_value("mV").item()
-            measured_current = self.ad_converter.get_current(channel).to_value("uA").item()
-            msg = SISIVMsg(
-                time = time.time(), setting_voltage = setting_voltage, measured_voltage = measured_voltage, measured_current = measured_current, id = id
-            )
-            self.publisher_.publish(msg)
-            self.logger.info(f'SIS IV Data: setting voltage={setting_voltage}mV, measured voltage={measured_voltage}mV, measured current={measured_current}mA')
-
+            self.com.sis_bias("?", id=id + "_I")
+            self.com.sis_bias("?", id=id + "_V")
