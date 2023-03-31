9a2dd67758dd489fbf4dddfa9f0fd1fc239350c9
Tatsumi ISHIKAWA <112744259+TatsumiISHIKAWA@users.noreply.github.com>
2023-03-31 12:56:02 +0900

    #175 Flake8 modify

--- necst/procedures/observations/hot_monitor.py
+++ necst/procedures/observations/hot_monitor.py
@@ -2,6 +2,7 @@ from typing import Union
 
 from .observation_base import Observation
 
+
 class HotMonitor(Observation):
 
     observation_type = "HotMonitor"
@@ -10,7 +11,6 @@ class HotMonitor(Observation):
     # This source code is reducing data in 1/10.
 
     def run(self, integ_time: Union[int, float]) -> None:
-        
         self.com.record("reduce", nth=10)
         self.logger.info(f"Starting {integ_time}hours Hot Monitor")
         self.integ_sec = integ_time*3600
