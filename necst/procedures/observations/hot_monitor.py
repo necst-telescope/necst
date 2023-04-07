2769589351a39d6c18b1b33b6e741919044035dd
Tatsumi ISHIKAWA <112744259+TatsumiISHIKAWA@users.noreply.github.com>
2023-04-08 01:16:12 +0900

    #179 Add new argment: record rate

--- necst/procedures/observations/hot_monitor.py
+++ necst/procedures/observations/hot_monitor.py
@@ -4,14 +4,13 @@ from .observation_base import Observation
 
 
 class HotMonitor(Observation):
-
     observation_type = "HotMonitor"
 
     # TODO: Add monitoring mode argument. -> Total power or Average(Reduce data).
-    # This source code is reducing data in 1/10.
 
-    def run(self, integ_time: Union[int, float]) -> None:
-        self.com.record("reduce", nth=10)
+    def run(self, integ_time: Union[int, float], rate: Union[int, float]) -> None:
+        conv_rate = rate * 0.1
+        self.com.record("reduce", nth=conv_rate)
         self.logger.info(f"Starting {integ_time}hours Hot Monitor")
         self.integ_sec = integ_time * 3600
         self.hot(self.integ_sec, f"{integ_time}h")
