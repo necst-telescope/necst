de30ea1a11be4601fe2c6785115547dd578027b0
Yamato Matsuzuki <t.fuuna@a.phys.nagoya-u.ac.jp>
2023-04-14 02:51:44 +0000

    #182 retry

--- necst/rx/sis_bias.py
+++ necst/rx/sis_bias.py
@@ -2,7 +2,7 @@ import time
 from typing import Dict
 
 from neclib.devices import BiasReader as SisBiasReader
-from neclib.devices import SisBiasSetter
+from neclib.devices import BiasSetter as SisBiasSetter
 from necst_msgs.msg import SISBias as SISBiasMsg
 from rclpy.publisher import Publisher
 
