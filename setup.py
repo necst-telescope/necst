68dcdf679c9e58cf9ded49aa1b026893d4c88ba9
Rin Yamada <yamadier@icloud.com>
2023-05-30 19:37:00 +0900

    #207 Update setup.py

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ executor_entrypoints = get_executor_entrypoints()
 
 setup(
     name=package_name,
-    version="0.6.4.1",
+    version="0.6.5",
     packages=find_packages(),
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
