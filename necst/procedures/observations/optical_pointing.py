80bd0e597dae0ffc94287878417c51ead2f30cbb
Ryo <64296621+Kirby1717@users.noreply.github.com>
2023-11-07 10:14:51 +0900

    #222 Add method about writing capture list

--- necst/procedures/observations/optical_pointing.py
+++ necst/procedures/observations/optical_pointing.py
@@ -1,5 +1,6 @@
 from datetime import datetime
-from typing import Optional, Tuple
+from typing import List, Optional, Tuple
+import os
 import time
 
 from neclib import config
@@ -10,6 +11,27 @@ from .observation_base import Observation
 
 class OpticalPointing(Observation):
     observation_type = "OpticalPointing"
+
+    def write_capture_list(
+        self,
+        directory: str,
+        filename: str,
+        az: List[float],
+        el: List[float],
+        pic_filename: List[str],
+        ra: List[float],
+        dec: List[float],
+        time: List[float],
+    ) -> None:
+        os.makedirs(directory, exist_ok=True)
+        with open(filename, mode="w") as file:
+            for _az, _el, _pic, _ra, _dec, _time in zip(
+                az, el, pic_filename, ra, dec, time
+            ):
+                file.write(
+                    f"{str(_az)} {str(_el)} {_pic} "
+                    f"{str(_ra)} {str(_dec)} {str(_time)}\n"
+                )
 
     def run(
         self,
@@ -44,9 +66,12 @@ class OpticalPointing(Observation):
         save_directory = config.ccd_controller.pic_captured_path / obsdatetime.strftime(
             "%Y%m%d_%H%M%S"
         )
-        az = []
-        el = []
-        pic_filename = []
+        cap_az = []
+        cap_el = []
+        cap_pic_filename = []
+        cap_ra = []
+        cap_dec = []
+        cap_time = []
         captured_num = 0
         try:
             for opt_target in sorted_list:
@@ -63,13 +88,18 @@ class OpticalPointing(Observation):
                     coord_before = self.com.antenna("?")
                     az_before = coord_before.lon
                     el_before = coord_before.lat
+                    time_before = time.time()
                     self.com.ccd("capture", name=save_path)
                     coord_after = self.com.antenna("?")
                     az_after = coord_after.lon
                     el_after = coord_after.lat
-                    az.append((az_before + az_after) / 2)
-                    el.append((el_before + el_after) / 2)
-                    pic_filename.append(save_filename)
+                    time_after = time.time()
+                    cap_az.append((az_before + az_after) / 2)
+                    cap_el.append((el_before + el_after) / 2)
+                    cap_pic_filename.append(save_filename)
+                    cap_ra.append(float(opt_target[1]))
+                    cap_dec.append(float(opt_target[2]))
+                    cap_time.append((time_before + time_after) / 2)
                     time.sleep(8.0)
                 captured_num += 1
                 self.logger.info(
@@ -84,10 +114,13 @@ class OpticalPointing(Observation):
             )
         finally:
             save_filename = obsdatetime.strftime("%Y%m%d_%H%M%S") + ".dat"
-            opt_pointing.write_capture_list(
+            self.write_capture_list(
                 directory=save_directory,
                 filename=save_filename,
-                az=az,
-                el=el,
-                pic_filename=pic_filename,
+                az=cap_az,
+                el=cap_el,
+                pic_filename=cap_pic_filename,
+                ra=cap_ra,
+                dec=cap_dec,
+                time=cap_time,
             )
