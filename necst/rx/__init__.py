565cacba443698ee377b0bbd2384689746a826c3
Takuto Ito <takutoito@Takuto-no-MacBook-Pro.local>
2023-11-15 15:56:34 +0900

    #238 fix import of rx

--- necst/rx/__init__.py
+++ necst/rx/__init__.py
@@ -1,7 +1,11 @@
 from .attenuator import AttenuatorController  # noqa: F401
-from .ccd import CCDController  # noqa: F401
 from .signal_generator import SignalGeneratorController  # noqa: F401
 from .sis_bias import SISBias  # noqa: F401
 from .hemt_bias import HEMTBias  # noqa: F401
 from .spectrometer import SpectralData  # noqa: F401
 from .thermometer import ThermometerController  # noqa: F401
+
+try:
+    from .ccd import CCDController  # noqa: F401
+except ImportError:
+    pass
