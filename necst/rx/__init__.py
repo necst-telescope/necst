c3b3d83e703111902102963b4edd425e17d08b0c
takayama-27 <t.fuuna@a.phys.nagoya-u.ac.jp>
2024-04-02 19:14:50 +0900

    #254 add init

--- necst/rx/__init__.py
+++ necst/rx/__init__.py
@@ -4,6 +4,7 @@ from .sis_bias import SISBias  # noqa: F401
 from .hemt_bias import HEMTBias  # noqa: F401
 from .spectrometer import SpectralData  # noqa: F401
 from .thermometer import ThermometerController  # noqa: F401
+from .powermeter import PowermeterController  # noqa: F401
 
 try:
     from .ccd import CCDController  # noqa: F401
