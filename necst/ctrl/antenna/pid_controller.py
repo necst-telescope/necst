06a9ae73e0eb3850860d550c37282195202e18dc
Takuto Ito <t.ito@a.phys.nagoya-u.ac.jp>
2024-05-08 09:39:17 +0900

    #252 add speed log

--- necst/ctrl/antenna/pid_controller.py
+++ necst/ctrl/antenna/pid_controller.py
@@ -186,6 +186,8 @@ class AntennaPIDController(AlertHandlerNode):
                 cmd_lat=cmd.lat,
                 enc_lon=enc.lon,
                 enc_lat=enc.lat,
+                az_speed=az_speed,
+                el_speed=el_speed,
                 time=cmd_time,
             )
 
@@ -211,25 +213,10 @@ class AntennaPIDController(AlertHandlerNode):
         self.discard_outdated_commands()
         now = pytime.time()
 
-        # Check if any command is available.
-        # if len(self.command_list) == 0:
-        #     self.immediate_stop_no_resume()
-        #     return
-
         # Check if command for immediate future exists or not.
         # if self.command_list[0].time > now + 2 / config.antenna_command_frequency:
         #     print(f"command zikann: {self.command_list}")
         #     return
-
-        # if (len(self.command_list) == 1) and (self.command_list[0].time > now - 1):
-        #     cmd = deepcopy(self.command_list[0])
-        #     if now - cmd.time > 1 / config.antenna_command_frequency:
-        #         cmd.time = now  # Not a real-time command.
-        # elif len(self.command_list) == 1:
-        #     cmd = self.command_list[0]
-        #     cmd.time = now
-        # else:
-        #     cmd = self.command_list[0]
 
         enc = self.enc[0]
         next_cmd = self.interpolated_command_reading(enc.time)
