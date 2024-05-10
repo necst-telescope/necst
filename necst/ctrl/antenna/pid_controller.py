fcebf0089a8246e07e117300ab1173de0e4ffab3
Takuto Ito <t.ito@a.phys.nagoya-u.ac.jp>
2024-05-10 10:16:05 +0900

    #252 add initail command

--- necst/ctrl/antenna/pid_controller.py
+++ necst/ctrl/antenna/pid_controller.py
@@ -252,7 +252,23 @@ class AntennaPIDController(AlertHandlerNode):
             self.gc.trigger()
             return
 
-        cmd = self.command_list.pop(0)
+        self.discard_outdated_commands()
+        now = pytime.time()
+
+        # Check if command for immediate future exists or not.
+        if self.command_list[0].time > now + 2 / config.antenna_command_frequency:
+            return
+
+        if (len(self.command_list) == 1) and (self.command_list[0].time > now - 1):
+            cmd = deepcopy(self.command_list[0])
+            if now - cmd.time > 1 / config.antenna_command_frequency:
+                cmd.time = now  # Not a real-time command.
+        elif len(self.command_list) == 1:
+            cmd = self.command_list.pop(0)
+            cmd.time = now
+        else:
+            cmd = self.command_list.pop(0)
+
         enc = self.command_list[0]
 
         try:
