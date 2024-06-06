585d86f2c3ef53e7c3d426fb8557ae522fb72317
Tada-academic <tada.kota.a9@s.gifu-u.ac.jp>
2024-06-06 15:30:08 +0900

    #259 add vacuum

--- necst/rx/__init__.py
+++ necst/rx/__init__.py
@@ -1,10 +1,11 @@
-from .attenuator import AttenuatorController  # noqa: F401
+rom .attenuator import AttenuatorController  # noqa: F401
 from .local_attenuator import LocalAttenuatorController  # noqa: F401
 from .signal_generator import SignalGeneratorController  # noqa: F401
 from .sis_bias import SISBias  # noqa: F401
 from .hemt_bias import HEMTBias  # noqa: F401
 from .spectrometer import SpectralData  # noqa: F401
 from .thermometer import ThermometerController  # noqa: F401
+from .vacuum_gauge import VacuumGaugeController  # noqa: F401
 
 try:
     from .ccd import CCDController  # noqa: F401
