09ea536c6eea08250160b7b396ab7cd5efb5444c
Tada-academic <tada.kota.a9@s.gifu-u.ac.jp>
2024-06-06 17:56:10 +0900

    #259 Fix

--- necst/definitions.py
+++ necst/definitions.py
@@ -8,7 +8,8 @@ from rclpy.qos import (
     HistoryPolicy,
     LivelinessPolicy,
     QoSProfile,
-    ReliabilityPolicy)
+    ReliabilityPolicy,
+)
 
 
 logger = get_logger(__name__)
@@ -198,8 +199,10 @@ class topic:
     obsmode = Topic(ObservingMode, "observing_mode", qos.realtime, namespace.core)
     channel_binning = Topic(Binning, "channel_binning", qos.reliable, namespace.rx)
     powermeter = Topic(DeviceReading, "powermeter", qos.realtime, namespace.rx, True)
-    vacuum_gauge = Topic(DeviceReading, "vacuum_gauge", qos.realtime, namespace.rx, True)
-
+    vacuum_gauge = Topic(
+        DeviceReading, "vacuum_gauge", qos.realtime, namespace.rx, True
+    )
+    
 class service:
     from necst_msgs.srv import (
         AuthoritySrv,
