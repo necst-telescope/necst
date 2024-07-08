d8e25ba885ad4b41d1789f9f3f520366ac480497
Takuto Ito <t.ito@a.phys.nagoya-u.ac.jp>
2024-07-08 17:12:09 +0900

    #252 fix time

--- necst/ctrl/antenna/pid_controller.py
+++ necst/ctrl/antenna/pid_controller.py
@@ -157,7 +157,7 @@ class AntennaPIDController(AlertHandlerNode):
 
             az_speed = float(self.decelerate_calc["az"](enc.lon, _az_speed))
             el_speed = float(self.decelerate_calc["el"](enc.lat, _el_speed))
-            msg = TimedAzElFloat64(az=az_speed, el=el_speed, time=enc.time)
+            msg = TimedAzElFloat64(az=az_speed, el=el_speed, time=pytime.time())
 
             log = CalcLog(
                 cmd_lon=exted_lon,
