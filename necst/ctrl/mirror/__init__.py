fd39151390a44d19ae96c00b1602a5ca949f6add
takayama-27 <t.fuuna@a.phys.nagoya-u.ac.jp>
2024-07-17 16:55:34 +0900

    init

--- necst/ctrl/mirror/__init__.py
+++ necst/ctrl/mirror/__init__.py
@@ -1,2 +1,2 @@
-from .mirror import M2Controller  # noqa: F401
-from .mirror import M4Controller  # noqa: F401
+from .m2 import M2Controller  # noqa: F401
+from .m4 import M4Controller  # noqa: F401
