21724987b2fba9145eba562083fe459808b2810c
takayama-27 <t.fuuna@a.phys.nagoya-u.ac.jp>
2024-07-17 13:18:36 +0900

    init

--- necst/core/__init__.py
+++ necst/core/__init__.py
@@ -5,3 +5,4 @@ from .commander import Commander  # noqa: F401
 from .device_node import DeviceNode  # noqa: F401
 from .recorder import RecorderController  # noqa: F401
 from .com_delay_test import ComDelayTest  # noqa: F401
+from .com_delay_test_topic import ComDelayTestTopic  # noqa: F401
