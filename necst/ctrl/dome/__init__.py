d497bd937f9f0b1a81cef5074bbaedba8374b7fb
Takuto Ito <t.ito@a.phys.nagoya-u.ac.jp>
2024-08-01 15:23:23 +0900

    #258 fix typo

--- necst/ctrl/dome/__init__.py
+++ necst/ctrl/dome/__init__.py
@@ -2,5 +2,5 @@ from .encoder import DomeEncoderController  # noqa: F401
 from .motor import DomeMotor  # noqa: F401
 from .pid_controller import DomePIDController  # noqa: F401
 from .sim_devices import DomeDeviceSimulator  # noqa: F401
-from .dome_tracking import DomeTrackingStatus  # nqoa: F401
-from .dome_controller import DomeController  # nqoa: F401
+from .dome_tracking import DomeTrackingStatus  # noqa: F401
+from .dome_controller import DomeController  # noqa: F401
