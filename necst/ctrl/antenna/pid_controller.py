a8efdb0353c8c983f737b29c38406e10d631ce17
Takuto Ito <t.ito@a.phys.nagoya-u.ac.jp>
2024-08-06 21:56:11 +0900

    #252 delete unused import

--- necst/ctrl/antenna/pid_controller.py
+++ necst/ctrl/antenna/pid_controller.py
@@ -3,7 +3,6 @@ from copy import deepcopy
 from typing import List
 
 from neclib.controllers import PIDController
-from neclib.data import LinearInterp, LinearExtrapolate
 from neclib.safety import Decelerate
 from neclib.utils import ParameterList
 from necst_msgs.msg import CoordMsg, PIDMsg, TimedAzElFloat64
