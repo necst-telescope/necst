9b326e4754ecf97642ba1df00243455a425de559
Takuto Ito <t.ito@a.phys.nagoya-u.ac.jp>
2024-08-07 01:34:44 +0900

    #252 debug

--- necst/ctrl/antenna/pid_controller.py
+++ necst/ctrl/antenna/pid_controller.py
@@ -152,6 +152,7 @@ class AntennaPIDController(AlertHandlerNode):
             cmd_time = enc.time
             msg = TimedAzElFloat64(az=az_speed, el=el_speed, time=cmd_time)
             self.command_publisher.publish(msg)
+            print(msg.az)
 
         except ZeroDivisionError:
             self.logger.debug("Duplicate command is supplied.")
--- tests/ctrl/test_antenna_controller.py
+++ tests/ctrl/test_antenna_controller.py
@@ -55,7 +55,7 @@ class TestAntennaController(TesterNode):
                 assert time.time() < timelimit, "Speed command not published in 1s"
                 az_condition = (speed_az is not None) and (speed_az > 0)
                 el_condition = (speed_el is not None) and (speed_el > 0)
-                print(speed_el)
+                print(speed_az)
                 if az_condition and el_condition:
                     break
                 time.sleep(0.02)
