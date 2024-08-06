99e37f1c763cec3bcf7e2cbfbaf9691e281d646a
Takuto Ito <t.ito@a.phys.nagoya-u.ac.jp>
2024-08-07 01:50:34 +0900

    #252 debug

--- tests/ctrl/test_antenna_controller.py
+++ tests/ctrl/test_antenna_controller.py
@@ -22,6 +22,7 @@ class TestAntennaController(TesterNode):
 
     def test_speed_is_published(self):
         controller = AntennaPIDController()
+        print(controller.pid_param)
 
         speed_az = speed_el = None
 
