54368f564e8a327257639a0800760f24615ddea4
OkamotoYuito <sfc05023@st.osakafu-u.ac.jp>
2025-01-24 18:37:49 +0900

    #336 refactoring tp_mode command

--- necst/core/commander.py
+++ necst/core/commander.py
@@ -1038,12 +1038,12 @@ class Commander(PrivilegedNode):
                 self.quick_look("ch", range=(0, ch), integ=1)
             return self.publisher["channel_binning"].publish(msg)
         elif CMD == "TP_MODE":
-            self.tp_mode = tp_mode
-            self.tp_range = tp_range
-            if tp_mode is None and tp_range == []:
-                msg = TPModeMsg(tp_mode=True, tp_range=[])
-            elif tp_mode or tp_range:  # tp_mode becomes True when tp_range is not empty
-                msg = TPModeMsg(tp_mode=True, tp_range=tp_range)
+            self.tp_mode = tp_mode if tp_mode is not None else self.tp_mode
+            self.tp_range = tp_range if tp_range else self.tp_range
+            if tp_range and (len(tp_range) != 2 or tp_range[0] > tp_range[1]):
+                raise ValueError("Invalid range")
+            if tp_mode or tp_range:  # tp_mode becomes True when tp_range is not empty
+                msg = TPModeMsg(tp_mode=True, tp_range=self.tp_range)
             else:
                 msg = TPModeMsg(tp_mode=False, tp_range=[])
             return self.publisher["tp_mode"].publish(msg)
