58482139d45e8a98544b8a94b02e75431089cbd7
Tatsumi ISHIKAWA <112744259+TatsumiISHIKAWA@users.noreply.github.com>
2025-03-14 11:40:25 +0900

    #348 add node

---
+++ necst/executors/RX.py
@@ -0,0 +1,44 @@
+import rclpy
+from rclpy.executors import MultiThreadedExecutor
+
+from ..rx.attenuator import AttenuatorController
+from ..rx.hemt_bias import HEMTBias
+from ..rx.local_attenuator import LocalAttenuatorController
+from ..rx.signal_generator import SignalGeneratorController
+from ..rx.sis_bias import SISBias
+from ..rx.thermometer import ThermometerController
+from ..rx.vacuum_gauge import VacuumGaugeController
+
+
+def configure_executor() -> MultiThreadedExecutor:
+    executor = MultiThreadedExecutor()
+    nodes = [
+        AttenuatorController(),
+        HEMTBias(),
+        LocalAttenuatorController(),
+        SignalGeneratorController(),
+        SISBias(),
+        ThermometerController(),
+        VacuumGaugeController(),
+    ]
+    _ = [executor.add_node(n) for n in nodes]
+    return executor
+
+
+def main(args=None) -> None:
+    rclpy.init(args=args)
+
+    executor = configure_executor()
+
+    try:
+        executor.spin()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        executor.shutdown()
+        _ = [n.destroy_node() for n in executor.get_nodes()]
+        rclpy.try_shutdown()
+
+
+if __name__ == "__main__":
+    main()
