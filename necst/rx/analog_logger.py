74dbf4330cb410c60d9b7fcf4280ffe9623faa6b
Tatsumi ISHIKAWA <112744259+TatsumiISHIKAWA@users.noreply.github.com>
2025-03-16 22:59:41 +0900

    #343 change method

--- necst/rx/analog_logger.py
+++ necst/rx/analog_logger.py
@@ -3,11 +3,12 @@ from typing import Dict
 
 from neclib.devices import AnalogLogger
 from necst_msgs.msg import DeviceReading
-from necst_msgs.msg import SISBias as SISBiasMsg
+from necst_msgs.msg import HEMTBias as HEMTBiasMsg
 from rclpy.publisher import Publisher
 
 from .. import namespace, topic
 from ..core import DeviceNode
+from . import channel_parser
 
 
 class AnalogLoggerController(DeviceNode):
@@ -19,63 +20,57 @@ class AnalogLoggerController(DeviceNode):
 
         self.logger = self.get_logger()
         self.io = AnalogLogger()
-        self.sis_channel = [
-            id for id in self.io.Config.channel.keys() if id.startswith("sis")
+        self.measure_channel = [
+            id for id in self.io.Config.channel.keys() if not id.startswith("sis")
         ]
-        self.hemt_channel = [
-            id for id in self.io.Config.channel.keys() if id.startswith("hemt")
-        ]
+        self.hemt_channel = [id for id in self.measure_channel if id.startswith("hemt")]
         self.other_channel = [
-            id
-            for id in self.io.Config.channel.keys()
-            if not (id.startswith("sis") | id.startwith("hemt"))
+            id for id in self.measure_channel if not id.startwith("hemt")
         ]
         self.publisher: Dict[str, Publisher] = {}
 
         self.create_timer(1, self.stream)
         self.create_timer(1, self.check_publisher)
-        if len(self.sis_channel) != 0:
-            from neclib.devices import SisBiasSetter
-
-            self.setter_io = SisBiasSetter()
-            topic.sis_bias_cmd.subscription(self, self.set_voltage)
+        self.logger.warning("SIS Tuning and Measuring are no implemented in this Node.")
+        self.logger.warning("Please use `sis_bias` Node to control SIS Bias.")
+        # TODO: Implement SIS Control. Just measuring Bias Parameters...
         self.logger.info(f"Started {self.NodeName} Node...")
         time.sleep(0.5)
         for key in self.measure_channel:
-            self.logger.info(f"{key}: {self.io.get_all(key)[key]}")
-
-    def set_voltage(self, msg: SISBiasMsg) -> None:
-        if msg.finalize:
-            self.setter_io.finalize()
-            return
-        else:
-            keys = self.setter_io.keys()
-            if None in keys:
-                for id in msg.id:
-                    self.setter_io.set_voltage(mV=msg.voltage, id=id)
-            else:
-                for id in msg.id:
-                    for key in keys:
-                        ch = self.setter_io[key].Config.channel.keys()
-                        if id in ch:
-                            self.setter_io[key].set_voltage(mV=msg.voltage, id=id)
-                            break
-                        else:
-                            continue
-            self.setter_io.apply_voltage()
-            self.logger.info(f"Set voltage {msg.voltage} mV for ch {msg.id}")
+            self.logger.info(f"{key}: {self.io.get_from_id(key)}")
             time.sleep(0.01)
 
     def check_publisher(self) -> None:
-        for name in self.measure_channel:
+        for name in self.other_channel:
             if name not in self.publisher:
                 self.publisher[name] = topic.analog_logger[name].publisher(self)
+        for name in self.hemt_channel:
+            if name not in self.publisher:
+                self.publisher[name] = topic.hemt_bias[name].publisher(self)
 
     def stream(self) -> None:
-        for name, publisher in self.publisher.items():
-            value = self.io.get_all(name)[name].item().value
+        channels = set(map(lambda x: x[:-4], self.hemt_channel))
+        data = self.reader_io.get_all("hemt")
+        for id in channels:
+            v_drain = data[f"{id}_Vdr"].to_value("V").item()
+            v_gate1 = data[f"{id}_Vg1"].to_value("V").item()
+            v_gate2 = data[f"{id}_Vg2"].to_value("V").item()
+            i_drain = data[f"{id}_Idr"].to_value("mA").item()
+            msg = HEMTBiasMsg(
+                time=time.time(),
+                v_drain=v_drain,
+                v_gate1=v_gate1,
+                v_gate2=v_gate2,
+                i_drain=i_drain,
+                id=id,
+            )
+            self.publisher[id].publish(msg)
+            time.sleep(0.01)
+        for name in self.other_channel:
+            value = self.io.get_from_id(name).item()
             msg = DeviceReading(time=time.time(), value=value, id=name)
-            publisher.publish(msg)
+            self.publisher[name].publish(msg)
+            time.sleep(0.01)
 
 
 def main(args=None):
