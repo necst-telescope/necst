0a681b7bd2e894928258fca3f71c5454d0d376be
OkamotoYuito <sfc05023@st.osakafu-u.ac.jp>
2025-05-26 11:28:37 +0900

    #385 fix qos in create_sub from definition.py

--- necst/rx/thermometer_test.py
+++ necst/rx/thermometer_test.py
@@ -1,20 +1,20 @@
 import rclpy
 from rclpy.node import Node
 from necst_msgs.msg import DeviceReading
-from rclpy.qos import qos_profile_sensor_data
+from .. import qos
 
 
 class ThermometerSubscriber(Node):
     def __init__(self):
         super().__init__("thermometer_test")
 
-        topic_to_subscribe = "/necst/OMU1P85M/rx/thermometer/Shield40K1"
+        topic_name = "/necst/OMU1P85M/rx/thermometer/Shield40K1"
 
         self.subscription = self.create_subscription(
             DeviceReading,
-            topic_to_subscribe,
+            topic_name,
             self.listener_callback,
-            qos_profile_sensor_data,
+            qos.realtime,
         )
 
     def listener_callback(self, msg: DeviceReading):
@@ -31,10 +31,9 @@ def main(args=None):
     except KeyboardInterrupt:
         pass
     finally:
-        if node and rclpy.ok() and not node.is_shutdown:
-            node.destroy_node()
-        if rclpy.ok():
-            rclpy.try_shutdown()
+        node.io.close()
+        node.destroy_node()
+        rclpy.try_shutdown()
 
 
 if __name__ == "__main__":
