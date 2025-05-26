c3ee997ef065b44b741fbd0e06d26f1d0ac739d7
OkamotoYuito <sfc05023@st.osakafu-u.ac.jp>
2025-05-26 15:43:52 +0900

    #385 test multi subs

--- necst/rx/thermometer_test.py
+++ necst/rx/thermometer_test.py
@@ -3,6 +3,8 @@ from necst_msgs.msg import DeviceReading
 from ..core import DeviceNode
 from .. import qos, namespace
 
+# import sqlite3
+
 
 class ThermometerSubscriber(DeviceNode):
     NodeName = "thermometer_test"
@@ -11,19 +13,38 @@ class ThermometerSubscriber(DeviceNode):
     def __init__(self):
         super().__init__(self.NodeName, namespace=self.Namespace)
 
-        topic_name = "/necst/OMU1P85M/rx/thermometer/Shield40K1"
+        name = "/necst/OMU1P85M/rx/thermometer/"
+        topic_names = ["Shield40K1", "Shield40K2", "Shield4K1", "Shield4K2"]
+        self.data_dict = {}
 
-        self.subscription = self.create_subscription(
-            DeviceReading,
-            topic_name,
-            self.listener_callback,
-            qos.realtime,
-        )
+        for topic_name in topic_names.split(","):
+            self.subscription = self.create_subscription(
+                DeviceReading,
+                f"{name}/{topic_name}",
+                self.listener_callback,
+                qos.realtime,
+            )
 
     def listener_callback(self, msg: DeviceReading):
+        self.data_dict[msg.id] = msg.value
         print(
             f"Received - ID: {msg.id}, Value: {msg.value:.2f} K, Time: {msg.time:.0f}"
         )
+
+    #     self.record_db(msg)
+
+    # def record_db(self, msg: DeviceReading):
+    #     conn = sqlite3.connect("thermometer_data.db")
+    #     cursor = conn.cursor()
+    #     cursor.execute(
+    #         "CREATE TABLE IF NOT EXISTS readings (id TEXT, value REAL, time REAL)"
+    #     )
+    #     cursor.execute(
+    #         "INSERT INTO readings (id, value, time) VALUES (?, ?, ?)",
+    #         (msg.id, msg.value, msg.time),
+    #     )
+    #     conn.commit()
+    #     conn.close()
 
 
 def main(args=None):
