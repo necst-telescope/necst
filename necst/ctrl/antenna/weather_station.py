28a625928f5cf07d36acdc6c44e275ab2a0e1fe4
masahirokato0116 <masakato@a.phys.nagoya-u.ac.jp>
2025-07-07 15:53:11 +0900

    #389 Add method to dictate multiple thermometers

--- necst/ctrl/antenna/weather_station.py
+++ necst/ctrl/antenna/weather_station.py
@@ -1,8 +1,10 @@
 import time
+from typing import Dict
 
 import rclpy
 from neclib.devices import WeatherStation
 from necst_msgs.msg import WeatherMsg
+from rclpy.publisher import Publisher
 
 from ... import namespace, topic
 from ...core import DeviceNode
@@ -13,21 +15,35 @@ class WeatherStationReader(DeviceNode):
     Namespace = namespace.root
 
     def __init__(self):
-        super().__init__(self.NodeName, namespace=self.Namespace)
+        try:
+            super().__init__(self.NodeName, namespace=self.Namespace)
 
-        self.publisher = topic.weather.publisher(self)
+            self.logger = self.get_logger()
+            self.io = WeatherStation()
 
-        self.thermo = WeatherStation()
-        self.create_timer(1, self.stream)
+            self.publisher: Dict[str, Publisher] = {}
+            self.create_safe_timer(1, self.check_publisher)
+            self.create_timer(1, self.stream)
 
-    def stream(self):
-        msg = WeatherMsg(
-            temperature=float(self.thermo.get_temperature().to_value("K")),
-            pressure=float(self.thermo.get_pressure().to_value("hPa")),
-            humidity=float(self.thermo.get_humidity()),
-            time=time.time(),
-        )
-        self.publisher.publish(msg)
+        except Exception as e:
+            self.logger.error(f"{self.NodeName} Node is shutdown due to Exception: {e}")
+            self.destroy_node()
+
+    def check_publisher(self) -> None:
+        for name in self.io.keys():
+            if name not in self.publisher:
+                self.publisher[name] = topic.lo_signal[name].publisher(self)
+
+    def stream(self) -> None:
+        for name, publisher in self.publisher.items():
+            msg = WeatherMsg(
+                temperature=float(self.io[name].get_temperature().to_value("K")),
+                pressure=float(self.io[name].get_pressure().to_value("hPa")),
+                humidity=float(self.io[name].get_humidity()),
+                time=time.time(),
+                id=name,
+            )
+            publisher.publish(msg)
 
 
 def main(args=None):
