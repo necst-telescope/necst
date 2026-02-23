3656dff34538173210283a25818210fdfaab8cfa
OkamotoYuito <sfc05023@st.osakafu-u.ac.jp>
2026-02-23 15:47:47 +0900

    #396 fix sg

--- necst/rx/signal_generator.py
+++ necst/rx/signal_generator.py
@@ -24,8 +24,7 @@ class SignalGeneratorController(DeviceNode):
             self.create_safe_subscription(topic.lo_signal_cmd, self.set_param)
             self.create_safe_timer(1, self.stream)
             self.create_safe_timer(1, self.check_publisher)
-            for name in self.io.keys():
-                self.io[name].create_safe_timer(1, self.check_status)
+            self.create_safe_timer(1, self.check_status)
             self.logger.info(f"Started {self.NodeName} Node...")
             for key in self.io.keys():
                 self.logger.info(
