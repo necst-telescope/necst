b044e0c01c76307656a5df8120a3ae4caee2b476
Takuto Ito <t.ito@a.phys.nagoya-u.ac.jp>
2026-02-24 18:28:14 +0900

    #408 fix

--- tests/core/test_commander.py
+++ tests/core/test_commander.py
@@ -105,9 +105,7 @@ class TestCommander(TesterNode):
         dev.enc.position.el = 44.0
 
         cmd = {"target": (30.0, 45.0, "altaz"), "unit": "deg"}
-        print(cmd, com)
         with spinning([auth_server, horizontal, pid, dev, tracking], n_thread=6):
-            print("in spin")
             com.get_privilege()
             com.antenna("point", **cmd, wait=True)
             com.quit_privilege()
