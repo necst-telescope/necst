a56e08d3db89a9189cf2b706a692afde07a8e87b
Takuto Ito <t.ito@a.phys.nagoya-u.ac.jp>
2026-02-24 17:40:56 +0900

    #408 flake8

--- tests/core/test_commander.py
+++ tests/core/test_commander.py
@@ -108,7 +108,7 @@ class TestCommander(TesterNode):
         dev.enc.position.az = 29.0
         dev.enc.position.el = 44.0
 
-        cmd = {"target": (30.0, 45.0, "altaz"), "unit": "deg"}    
+        cmd = {"target": (30.0, 45.0, "altaz"), "unit": "deg"}
         with spinning([auth_server, horizontal, pid, dev, tracking], n_thread=6):
             com.get_privilege()
             com.antenna("point", **cmd, wait=True)
