a15ce0dc8ad20a568579892a7772923333aa0644
OkamotoYuito <sfc05023@st.osakafu-u.ac.jp>
2026-06-05 12:25:28 +0900

    Revert "#474 fix: add bin/antenna-az-unwrap-state.py CLI entry point"
    This reverts commit 7c40b5db0c7a517099d55e24bf5168ee89a04ae0.

--- bin/antenna-az-unwrap-state.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""Inspect or initialize the NECST Az unwrap state file."""
-
-import sys
-
-from necst.ctrl.antenna.az_unwrap import main
-
-if __name__ == "__main__":
-    sys.exit(main())
